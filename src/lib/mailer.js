4f44f0c20f8f70417a3394579e833926dfa6c903
Dominiek Ter Heide <info@dominiek.com>
2018-02-21 10:59:50 -0800

    Console log

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -3,7 +3,7 @@ import postmark from 'postmark';
 
 export const sendMail = (config, { to, subject, body }) => {
   const client = new postmark.Client(config.postmark.apikey);
-  console.log('Sending email: ', to, subject, body);
+  // console.log('Sending email: ', to, subject, body);
   const env = process.env.NODE_ENV;
   if (env !== 'test') {
     client.sendEmail({
