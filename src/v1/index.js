778cf6c8421dcbf822b788f7b3c1570e0368e7de
Kaare Larsen <me@kaareal.com>
2018-03-07 21:48:07 +0100

    added test for index

--- src/v1/index.js
+++ src/v1/index.js
@@ -3,18 +3,12 @@ const Router = require('koa-router');
 const users = require('./users');
 const applicants = require('./applicants');
 const info = require('./info');
-const { version } = require('../../package.json');
 
 const router = new Router();
 
 router.use('/users', users.routes());
 router.use('/applicants', applicants.routes());
 router.use('/info', info.routes());
-
-router.get('/', (ctx) => {
-  const protocolVersion = 1;
-  ctx.body = { version, protocolVersion };
-});
 
 module.exports = router;
 
